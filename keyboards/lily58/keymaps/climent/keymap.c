8eb5a2eed313a16e255386aa177e404fe375c4a6
Jesus Climent <climent@gmail.com>
2021-11-06 04:20:53 -0400

    Changed behaviour of ENT key, LOCK SCREEN for MAC, and NEXT/PREV spaces.

--- keyboards/lily58/keymaps/climent/keymap.c
+++ keyboards/lily58/keymaps/climent/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,    KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                       MO(_LOWER), KC_LALT, KC_LGUI, KC_ENT ,    KC_SPC ,  MO(_RAISE), KC_ENT, KC_RCTRL \
+                       MO(_LOWER), KC_LALT, KC_LGUI, LCTL_T(KC_ENT) ,    KC_SPC ,  MO(_RAISE), KC_ENT, KC_RCTRL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(\
-  _______, _______, _______, _______, _______, _______,                      KC_HOME, KC_END , _______, KC_LCBR, KC_RCBR, KC_TILD, \
-  _______, _______, _______, _______, _______, _______,                      KC_PGUP, XXXXXXX, KC_UP  , KC_LBRC, KC_RBRC, KC_DEL , \
-  _______, _______, _______, _______, _______, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PIPE, KC_ENT , \
-  _______, _______, _______, _______, _______, _______, KC_HOME, KC_END    , KC_UNDS, KC_MINS, KC_EQL , KC_PLUS, KC_BSLS, _______, \
-                             _______, _______, _______, KC_SPC , G(C(KC_Q)), _______, KC_RALT, KC_RGUI \
+  _______, _______, _______, _______, _______, _______,                    C(KC_LEFT), C(KC_RGHT) , _______, KC_LCBR, KC_RCBR, KC_TILD, \
+  _______, _______, _______, _______, _______, _______,                    KC_PGUP, XXXXXXX, KC_UP  , KC_LBRC, KC_RBRC, KC_DEL , \
+  _______, _______, _______, _______, _______, _______,                    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PIPE, KC_ENT , \
+  _______, _______, _______, _______, _______, _______, KC_HOME, KC_END  , KC_UNDS, KC_MINS, KC_EQL , KC_PLUS, KC_BSLS, G(C(KC_Q)), \
+                             _______, _______, _______, KC_SPC , _______ , _______, KC_RGUI, KC_RALT \
 ),
   /*_______, C(KC_1), C(KC_2), C(KC_3), C(KC_4), C(KC_5),                     KC_PGUP, XXXXXXX, KC_UP  , KC_LBRC, KC_RBRC, KC_DEL , \
   //_______, C(KC_6), C(KC_7), C(KC_8), C(KC_9), C(KC_0),                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PIPE, KC_ENT , \
