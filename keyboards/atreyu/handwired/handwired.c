a91ad6b332e5fd64e9fcabf5f1df68871f22d776
Jesus Climent <climent@gmail.com>
2022-07-14 07:12:09 +0100

    Update atreyu's structure to accommodate the rev1/handwire split.

--- keyboards/atreyu/handwired/handwired.c
+++ keyboards/atreyu/handwired/handwired.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "atreyu.h"
+#include "handwired.h"
 
 void matrix_init_kb(void) {
 	// put your keyboard start-up code here
